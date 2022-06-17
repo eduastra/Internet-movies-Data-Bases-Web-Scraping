d992c5d4eac928e7679460ea5bccb45853f02e71
eduastra <cadu_gold@hotmail.com>
2022-06-17 15:47:20 -0300

    rest para testes e modelo HTML para consulta

--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+from tkinter import W
 import requests
 from bs4 import BeautifulSoup
 
@@ -15,3 +16,6 @@ artist_name_list_items = artist_name_list.find_all('a')
 
 for artist_name in artist_name_list_items:
     print(artist_name.prettify())
+
+    with open ("base.txt", W) as arquivo:
+        print(artist_name.prettigy())
